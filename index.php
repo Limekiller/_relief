e0146c8bdbe91bff03b28f57c7e0fff8481a7b4f
Limekiller <me@bryceyoder.com>
2026-07-16 16:26:58 +0000

    Update colors and add pizazz!

--- index.php
+++ index.php
@@ -7,11 +7,23 @@ if ( has_post_thumbnail() ) {
 
 <div id='main-content' class="content <?php echo $featured_image ? 'has-image' : '' ?>">
     <?php if ( $featured_image ) : ?>
-        <img class="featured-image star" src="<?php echo $featured_image; ?>" />
+        <div class="featured-container">
+            <img class="featured-image" src="<?php echo $featured_image; ?>" />
+            <div class="featured-content-container" style="margin: 0">
+                <div class="header">
+                    <h1><?php echo get_the_title(); ?></h1>
+                </div>
+                <div class="featured-content">
+                    <?php echo get_field('featured_content'); ?>
+                </div>
+            </div>
+
+        </div>
+    <?php else : ?>
+        <div class="header">
+            <h1><?php echo get_the_title(); ?></h1>
+        </div>
     <?php endif; ?>
-    <div class="header <?php echo $featured_image ? "img-header" : "" ?>">
-        <h1><?php echo get_the_title(); ?></h1>
-    </div>
     <?php
         if ( have_posts() ) : while ( have_posts() ) : the_post();
         get_template_part( 'content', get_post_format() );
